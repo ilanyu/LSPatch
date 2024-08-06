05887c47c278602061d539423ce25193dc60ef67
ilanyu <helloshevon@gmail.com>
2024-08-06 10:56:47 +0800

    add QUERY_ALL_PACKAGES for all patch app

--- patch/src/main/java/org/lsposed/patch/LSPatch.java
+++ patch/src/main/java/org/lsposed/patch/LSPatch.java
@@ -330,9 +330,7 @@ public class LSPatch {
         property.addApplicationAttribute(new AttributeItem(NodeValue.Application.DEBUGGABLE, debuggableFlag));
         property.addApplicationAttribute(new AttributeItem("appComponentFactory", PROXY_APP_COMPONENT_FACTORY));
         property.addMetaData(new ModificationProperty.MetaData("lspatch", metadata));
-        // TODO: replace query_all with queries -> manager
-        if (useManager)
-            property.addUsesPermission("android.permission.QUERY_ALL_PACKAGES");
+        property.addUsesPermission("android.permission.QUERY_ALL_PACKAGES");
 
         var os = new ByteArrayOutputStream();
         (new ManifestEditor(is, os, property)).processManifest();
